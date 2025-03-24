e02386bc770c99fc550a563a3e1a8d57e0d8e501
vlady <vladysha1021@gmail.com>
2025-03-23 21:13:59 -0400

    Add emplace() and pop() methods to the Queue class

--- Lab3_Parallel_Computing/main.cpp
+++ Lab3_Parallel_Computing/main.cpp
@@ -4,6 +4,7 @@
 #include <condition_variable>
 #include <iostream>
 #include <queue>
+#include <map>
 
 using read_lock = std::shared_lock<std::shared_mutex>;
 using write_lock = std::unique_lock<std::shared_mutex>;
@@ -42,12 +43,47 @@ std::mutex Task::counterMutex;
 
 class Queue {
 private:
-    std::queue<Task> tasks;         
-
+    std::queue<Task> tasks;   
+    int totalQueueLength = 0;       
+    int measureCount = 0;
+    std::map<std::thread::id, long long> threadWaitTimes; 
     std::shared_mutex m_rw_lock;    
+    std::condition_variable_any condVar;   
+
+    void recordQueueLength() {
+        totalQueueLength += tasks.size();
+        measureCount++;
+    }
+    void recordThreadWaitTime(long long waitTime) {
+        threadWaitTimes[std::this_thread::get_id()] += waitTime;
+    }
+
 public:
     Queue() = default;
     ~Queue() { clear(); }
+
+    void emplace(const Task& task) {
+        write_lock lock(m_rw_lock);
+        tasks.emplace(task); 
+        recordQueueLength();
+        condVar.notify_one();
+    }
+
+    Task pop() {
+        write_lock lock(m_rw_lock);
+        auto startWait = std::chrono::high_resolution_clock::now();
+        while (tasks.empty()) {
+            condVar.wait(lock);
+        }
+        auto endWait = std::chrono::high_resolution_clock::now();
+        long long waitDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(endWait - startWait).count();
+        recordThreadWaitTime(waitDuration);
+
+        Task task = tasks.front();
+        tasks.pop();
+        recordQueueLength();
+        return task;
+    }
 
     bool empty() {
         read_lock lock(m_rw_lock);
